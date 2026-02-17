3f655afdc354ba605ab09435228916c787ac608c
abarrenx <abarrenx@SOC-PF41WAS9>
2026-02-17 13:18:48 -0600

    Se encripta la conexion de base datos

---
+++ UMCAEstilo/Controladores/CategoriaController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace UMCAEstilo.Controladores
+{
+    public class CategoriaController
+    {
+
+        private string connectionString;
+
+        public CategoriaController() {
+
+            connectionString = ConfigurationManager.ConnectionStrings["Conexion"].ConnectionString;//Obtener la cadena de conexion
+        
+        }
+
+    }
+}
